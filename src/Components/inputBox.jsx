814ee5867160bd225b3ff988275079c3adc0c4e6
Narasimha7076 <bablusomesula@gmail.com>
2024-06-13 14:22:12 +0530

    Modified InputBox

--- src/Components/inputBox.jsx
+++ src/Components/inputBox.jsx
@@ -17,8 +17,8 @@ function InputBox({
     return (
         <div className={`bg-white p-3 rounded-lg text-sm flex ${className}`}>
             <div className="w-1/2">
-                <label  className="text-black/40 mb-2 inline-block">
-                    {label}
+                <label  className="text-black/40  inline-block">
+                {label}
                 </label>
                 <input
                     
